858e9f231faaadc0bd4a5907cc73d8afb9a2435e
lurenzhong <lurenzhong@didichuxing.com>
2018-02-05 15:43:45 +0800

    实现家庭作业

--- decisionTree/python/homework.py
+++ decisionTree/python/homework.py
@@ -4,3 +4,23 @@
 """
     家庭作业代码实现
 """
+
+
+def homeWorkClassTest():
+    trainingFile = '../data/training.data'
+    testFile = '../data/test.data'
+
+    f = open(trainingFile)
+    t = open(testFile)
+
+    trainingData = [inst.strip().split(',') for inst in f.readlines()]
+
+    testingData = [inst.strip().split(',') for inst in t.readlines()]
+
+    print(trainingData)
+
+
+
+
+if __name__ == '__main__':
+    homeWorkClassTest()
