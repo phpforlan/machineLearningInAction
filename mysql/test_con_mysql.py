cc3752ce9656db47c7974a8fd5f69336d2f110ea
lurenzhong <lurenzhong@didichuxing.com>
2018-12-23 18:00:43 +0800

    数据库连接

--- mysql/test_con_mysql.py
+++ mysql/test_con_mysql.py
@@ -1,22 +1,55 @@
 import MySQLdb
 
-try:
-    conn = MySQLdb.connect(
-        host = '127.0.0.1',
-        user = 'root',
-        passwd = '123456',
-        port = 3306,
-        db = "news",
-        charset = 'utf8',
-        connect_timeout = 3
-    )
-    cursor = conn.cursor()
-    cursor.execute('select * from news')
-    result = cursor.fetchall()
+class MysqlSearch(object):
+    conn = None
 
-    print(len(result))
+    def __init__(self):
+        self.get_conn()
 
-except MySQLdb.Error as e:
-    print('Error: %s' % e)
+    # 获取数据库连接
+    def get_conn(self):
+        try:
+            self.conn = MySQLdb.connect(
+                host='127.0.0.1',
+                user='root',
+                passwd='123456',
+                port=3306,
+                db="news",
+                charset='utf8',
+                connect_timeout=3
+            )
+        except MySQLdb.Error as e:
+            print('Error: %s' % e)
 
-conn.close()
+    # 关闭数据库连接
+    def close_conn(self):
+        try:
+            if self.conn:
+                self.conn.close()
+        except MySQLdb.Error as e:
+            print('Error: %s' % e)
+
+    # 获取一条数据
+    def get_one(self):
+        sql = "select * from `news` where types = %s order by created_at desc"
+        cursor = self.conn.cursor()
+        cursor.execute(sql, ('百家',))
+        result = cursor.fetchone()
+
+        zipObj = zip()
+
+        for v in zipObj:
+            print(v)
+
+
+
+        self.close_conn()
+
+
+def main():
+    obj = MysqlSearch()
+    obj.get_one()
+
+
+if __name__ == '__main__':
+    main()
